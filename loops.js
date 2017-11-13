fef7673372e9262e26091c258416814e46f3a884
Endri Gina <endrigina90@gmail.com>
2017-11-13 18:35:24 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,4 +1,4 @@
 for (var i = 0; i < 25; i++) {
-  if i=1  console.log( "I am 1 strange loop." );
-  else console.log( "I am ${i} strange loops." )
-}
+  if (i<2)  {console.log( "I am 1 strange loop." )
+}  else { console.log( "I am ${i} strange loops." )
+} }
